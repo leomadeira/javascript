a1e3d7792da720dd8bc0414f144c67ad9f59888a
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 15:12:33 -0300

    up

---
+++ JavaScript/Fundamentos/var-let-const.js
@@ -0,0 +1,15 @@
+// Existe três tipos de variáveis(var, let e const)
+
+var num1 = 10 // Var está no escopo global
+
+let num2 = 20 // Let está no escopo local(Regra: sempre use let ao invés de usar 'var')
+
+const num3 = 30 // Caso não é necessário mudar o valor de uma variável, por boa prática é melhor usar 'const'
+
+/***********************************************************************************/
+
+var num1 = 50 // Podemos declarar o mesmo nome de uma var para substituir o valor da variavel
+num2 = 40 // não pode repetir o let, apenas colocar o num2 e o novo valor da variável, senão vai da erro no código
+//num3 = 50 // não dá para mudar o valor de uma constante, ela sempre será 30.
+
+console.log(num1,num2)
