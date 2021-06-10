16a8d91954933f336bb713c8aadda4ec0b3c3643
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 18:17:01 -0300

    up

---
+++ JavaScript/Fundamentos/math.js
@@ -0,0 +1,6 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio,2) // Math esta colocando o valor de PI=> (pi*5.6²) <= e jogando a potencia de 2 com o pow(potencia)
+
+console.log(area)
+console.log(area.toFixed(1))
+console.log(typeof Math) // Math é um objeto padrao do JS
---
+++ JavaScript/Fundamentos/strings.js
@@ -0,0 +1,23 @@
+const nome = 'Leonardo'
+
+console.log(nome.charAt(4)) // vai me da o 4° número da variável nome(letra a) // COMEÇA COM O NUMERO '0'
+console.log(nome.toUpperCase()) // TODAS AS LETRAS EM MAIÚSCULA 
+console.log(nome.toLowerCase()) // todas as letras em minúscula
+
+
+const nome2 = "Maria" //
+const nome3 = `José`  // Podemos usar tres sinais para declarar uma string. com apenas uma 'x', duas "x" e `x`(crase)
+//crase é o meu preferido para eu poder concatenar nem problemas as variaveis com outros elementos.
+
+console.log(nome3 + ' Monteiro') // Não curto muito dessa forma
+
+console.log(`${nome} Madeira`) // Dessa forma acho mais profissional e simples. Que é a forma que a maioria dos programadores usam.
+
+const upper = texto => texto.toUpperCase() // criei uma função que deixa qualquer palavra com letra maiuscula
+
+function lower(texto) {  // outra forma de criar função, deixando todas as letras em minusculo
+    return texto.toLowerCase()
+}
+
+console.log(upper(nome))
+console.log(lower(nome))
