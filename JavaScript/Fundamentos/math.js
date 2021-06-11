53a87b29cf26936b7c512b1a91bdd2e1743ee07c
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 23:07:55 -0300

    Math, importante aprender

--- JavaScript/Fundamentos/math.js
+++ JavaScript/Fundamentos/math.js
@@ -1,6 +1,9 @@
 const raio = 5.6
 const area = Math.PI * Math.pow(raio,2) // Math esta colocando o valor de PI=> (pi*5.6²) <= e jogando a potencia de 2 com o pow(potencia)
 
+const x = 3.9
+console.log(Math.ceil(x)) // arredonda o valor de x para cima 3.9 para 4
+
 console.log(area)
-console.log(area.toFixed(1))
+console.log(area.toFixed(1)) //toFixed reduz as casas decimais para 1 como esta no exemplo
 console.log(typeof Math) // Math é um objeto padrao do JS
