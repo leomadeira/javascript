6bcff336eed128c2a5123099fdff4fc2912ebab7
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-11 13:08:33 -0300

    Create destructuring2.js

---
+++ JavaScript/Fundamentos/destructuring2.js
@@ -0,0 +1,12 @@
+// destructuring com array que no caso é com []
+
+const [a] = [10] // array simples com destructuring
+console.log(a)
+
+const [b, c, d, e] = [4,6,7,2]
+console.log(b) // B pegou apenas o primeiro elemento
+// observe que desta forma, criamos varios nomes de viriaveis e varios dados. assim não precisamos criar varios consts.
+
+const [[x,y],[j,i]] = [[9,4],[5,3]] // aqui um ex simples de uma matriz
+console.log(x)
+console.log(i)
