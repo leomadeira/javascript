fdec78f6434bf319f11236323509dc0eafb09fcf
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 23:08:13 -0300

    Objeto 2

---
+++ JavaScript/Fundamentos/objeto2.js
@@ -0,0 +1,32 @@
+/************************* */
+function sobreNome(nome) {
+    console.log(`${nome} Madeira`)
+}
+
+sobreNome('Leonardo')
+
+console.log(typeof sobreNome)
+
+/************************* */
+
+const cliente = { nome:'José', idade:80, peso:70, altura: 1.55, endereco:{cidade:'Macapá', estado:'Amapá'} }
+console.log(typeof cliente);
+
+/************************* */
+
+const obj1 = {}
+obj1.nome = 'Léo'
+obj1.idade = 24
+console.log(obj1)
+
+/************************* */
+
+function NewObj(nome) {
+    this.nome = nome
+}
+newobj1 = new NewObj('Dayane')
+newobj2 = new NewObj('Carlos')
+console.log(newobj1)
+console.log(newobj2)
+
+/************************* */
