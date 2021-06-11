95277e7b83826b39de74e2e4ca63ab3e3109541e
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 23:08:04 -0300

    Objeto 1

---
+++ JavaScript/Fundamentos/objeto1.js
@@ -0,0 +1,7 @@
+const pessoa = { nome:'Leonardo', idade: 24 , peso: 70, altura: 1.69}
+
+console.log(pessoa.nome)
+
+const produto = { nome : 'Celular', preco: 1499.99}
+produto.desconto = produto.preco-(produto.preco*0.2).toFixed(2) // calculo para da um desconto de 20%
+console.log(produto)
