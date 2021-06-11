f1c2d55abff5deb2717afe5420a76aebeb565a31
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-11 18:44:23 -0300

    Update ternario.js

--- JavaScript/Fundamentos/ternario.js
+++ JavaScript/Fundamentos/ternario.js
@@ -1,2 +1,6 @@
 let [x,y,i,j] = [10,5,8,-4];
 console.log(x>=i?'Verdadeiro':'Falso') // Eu poderia criar um if e else, mas essa é um forma p reduzir o codigo.
+
+const bool = () => 7>=10?'Verdadeiro':'Falso' // tb funciona criando uma função e retornar o valor boolean
+console.log(bool())
+//cada valor pode ser
