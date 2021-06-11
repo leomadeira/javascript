b4d328001d6bb9fb4683adb5e1814019e6913863
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-11 13:08:30 -0300

    Update destructuring1.js

--- JavaScript/Fundamentos/destructuring1.js
+++ JavaScript/Fundamentos/destructuring1.js
@@ -1,3 +1,5 @@
+// destructuring com Objeto que no caso é com {}
+
 let produto = { nome:'celular', preco: 899.90} // obj simples
 const {nome,preco} = produto // const{} e coloca o nome da variavel que esta dentro do obj
 console.log(`Primeiro exemplo: ${nome}, ${preco}`)
@@ -15,4 +17,11 @@ console.log(`Segundo ex: ${n}, ${p}`)
 // Destructuring ele simplesmente extrai um valor de um objeto e transforma o valor do objeto em uma variavel, assim fica mais facil de chamar o valor. Para chamar o nome celular, eu teria que escrever produto.nome p/ printar o elemento que esta dentro de nome. 
 //ex: console.log(produto.nome)
 
-
+/********************************************/
+{// coloquei essa chave para constante ficar em um escopo diferente.Ja que existe duas variaveis com o mesmo nome(nome):p
+const pessoa = { nome:'Cesar', idade : 65, endereco:{rua:'av lorem ipsum',cidade:'Lorem', estado:'IS'}}
+const {endereco:{cidade:c,estado:e}} = pessoa // nesse ex, eu peguei duas variaveis dentro de um objeto
+const {nome} = pessoa
+console.log(nome)
+console.log(`Cidade: ${c}/${e}`)
+}
