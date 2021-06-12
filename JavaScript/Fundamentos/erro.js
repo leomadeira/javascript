be2a7973e8152ef6d3de1b22c55031184355c988
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-12 18:47:51 -0300

    Create erro.js

---
+++ JavaScript/Fundamentos/erro.js
@@ -0,0 +1,21 @@
+function tratarErro(erro) {
+    throw{
+        name: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNome(obj) {
+    try{ // try: caso o programa nao tenha erro, o programa é executado normalmente
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e){ // catch: caso o programa de erro, a função tratarErro é executada
+        tratarErro(e)
+    } finally{  // finally: mesmo o programa dando erro ou nao o comendo é executado.
+        console.log('Final do Programa!')
+    }
+}
+
+const obj = { n: 'Leonardo'} // Escreva name no lugar de N para o programa executar corretamente.
+
+imprimirNome(obj) 
