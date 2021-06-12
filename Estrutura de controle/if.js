dd517a59cbf1320ecdcd3d738f0749bb027b0680
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-12 19:11:30 -0300

    Update if.js

--- Estrutura de controle/if.js
+++ Estrutura de controle/if.js
@@ -1,7 +1,25 @@
 let x = 10
 
-if (x>=10) {
+
+/******************************************************************** */
+                        // COM FUNÇÃO
+
+function noticiaBoa(nota) { // DUAS FUNÇÕES COM ESTRUTRURA DE CONTROLE
+    if (nota>=7) {
+        console.log('APROVADO!')
+    }
+}
+
+function noticiaRuim(nota){
+    if (nota<=6) {
+        console.log('REPROVADO')
+    }
+}
+
+noticiaBoa(10)
+
+/******************************************************************** */
+
+if (x===10) {
     console.log('Hello')
-}else{
-    console.log('Coloca 10 la em cima, mané')
 }
