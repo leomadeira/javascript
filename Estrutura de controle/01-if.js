d2d42deefc8d509754a59de5e7c7221cac497af7
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-13 00:39:15 -0300

    Create 01-if.js

---
+++ Estrutura de controle/01-if.js
@@ -0,0 +1,25 @@
+let x = 10
+
+
+/******************************************************************** */
+                        // COM FUNÇÃO
+
+function noticiaBoa(nota) { // DUAS FUNÇÕES COM ESTRUTRURA DE CONTROLE
+    if (nota>=7) {
+        console.log('APROVADO!')
+    }
+}
+
+function noticiaRuim(nota){
+    if (nota<=6) {
+        console.log('REPROVADO')
+    }
+}
+
+noticiaBoa(10)
+
+/******************************************************************** */
+
+if (x===10) {
+    console.log('Hello')
+}
