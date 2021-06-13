fe99b6832c93938894b9146de4249e5ec9b94e8b
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-13 01:06:20 -0300

    Update 07 - for.js

--- Estrutura de controle/07 - for.js
+++ Estrutura de controle/07 - for.js
@@ -1,5 +1,14 @@
 let valor = 10
 while (valor<=10) {
-    console.log(`Valor: ${valor}`)
+ //   console.log(`Valor: ${valor}`)
     valor++
+}
+
+for (let i = 1; i < valor; i++) { // Podemos fazer a mesma coisa de um while, usando o For.
+ //   console.log(i)  
+}
+
+const array = ['Léo','Day','José','Pedro','Carlos'];
+for (let i = 0; i < array.length; i++) { // podemos printar todos os valores de uma array
+ //   console.log(array[i]);
 }
