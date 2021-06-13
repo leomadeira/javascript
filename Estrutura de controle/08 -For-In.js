a4ddcfb2a20e47915948e01f4914472f491326ae
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-13 01:44:22 -0300

    Create 08 -For-In.js

---
+++ Estrutura de controle/08 -For-In.js
@@ -0,0 +1,17 @@
+const object = [10,8,6,8,5,2] // for in com array
+
+for (const i in object) {
+//    console.log(object[i])
+}
+
+const Obj = { // for in com objeto
+    nome: 'Léo',
+    idade: '24 anos',
+    peso: '68kg',
+    altura: '1.69 cm'
+}
+
+for (const i in Obj) { // forma de printar um Objeto
+ // console.log(Obj[i])
+    console.log(`${i} = ${Obj[i]}`) // Desta forma fica mais elegante.
+}
