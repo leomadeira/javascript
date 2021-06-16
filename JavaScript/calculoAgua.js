93eb49c5fbc53a841461abde743c24c0c660e20d
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-06-15 22:28:24 -0300

    Update calculoAgua.js

--- JavaScript/calculoAgua.js
+++ JavaScript/calculoAgua.js
@@ -4,3 +4,11 @@ function CalcAgua(kg) {
     console.log(`Você precisa de ${total.toFixed(3)} L por dia`)
 }
 CalcAgua(200)
+
+function pitagoras(b,c){
+    let a = (b**2)+ (c**2)
+    a = a**0.5 // raizQuadrada
+    console.log(a)
+}
+
+pitagoras(3,5)
