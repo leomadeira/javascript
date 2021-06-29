4ca619b5311650a93e25b92c4a8a4ee889b66108
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-29 03:00:21 -0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,17 +1,2 @@
- let pessoa = {nome:'Leonardo', falar(){ 
-     console.log(`Olá, meu nome é ${this.nome}!`)
-    }
-}
- 
- let sobreNome = (nome) => {
-  console.log(`${nome} Madeira`)
-}
-
-const {nome} = pessoa
-console.log(nome)
-console.log(pessoa.nome)
-const {falar} = pessoa
-console.log(falar)
-pessoa.falar()
-
-sobreNome('Leonardo')
+let nome = 'Leonardo'
+console.log(nome.padStart())
