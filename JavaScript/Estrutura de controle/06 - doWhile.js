1795d9f49e1314b7b10478b83ccf299b106ce4f3
Leonardo <leomadeiraa@hotmail.com>
2021-07-12 23:05:36 -0300

    Update 06 - doWhile.js

--- JavaScript/Estrutura de controle/06 - doWhile.js
+++ JavaScript/Estrutura de controle/06 - doWhile.js
@@ -1,11 +1,12 @@
-let gerarNumeroAleatorio = (min,max) =>{
-    const valor = Math.random() * (max - min) + min
-    return Math.floor(valor)
+let gerarNumeroAleatorio = (min, max) => {
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor)
 }
 
 let laco = -1
 
- do { // a diferança do while para o doWhale, é o posição da tag, mas o do executa sempre mesmo primeiro mesmo se a condição ja for verdadeira. DoWhile nao é mt utilizado, o que os Devs mais usam é o for que vamos ver agora
-    laco = gerarNumeroAleatorio(-2,10)
-    console.log(laco)
+do {
+  // a diferança do while para o doWhale, é o posição da tag, mas o do executa sempre mesmo primeiro mesmo se a condição ja for verdadeira. DoWhile nao é mt utilizado, o que os Devs mais usam é o for que vamos ver agora
+  laco = gerarNumeroAleatorio(-2, 10)
+  console.log(laco)
 } while (laco != -1)
