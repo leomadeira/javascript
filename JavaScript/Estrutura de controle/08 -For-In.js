34283ebccc607b6e2baa1ce233cb8c725e093e6c
Leonardo <leomadeiraa@hotmail.com>
2021-07-12 23:05:42 -0300

    Update 08 -For-In.js

--- JavaScript/Estrutura de controle/08 -For-In.js
+++ JavaScript/Estrutura de controle/08 -For-In.js
@@ -1,17 +1,19 @@
-const object = [10,8,6,8,5,2] // for in com array
+const object = [10, 8, 6, 8, 5, 2] // for in com array
 
 for (const i in object) {
-//    console.log(object[i])
+  //    console.log(object[i])
 }
 
-const Obj = { // for in com objeto
-    nome: 'Léo',
-    idade: '24 anos',
-    peso: '68kg',
-    altura: '1.69 cm'
+const Obj = {
+  // for in com objeto
+  nome: 'Léo',
+  idade: '24 anos',
+  peso: '68kg',
+  altura: '1.69 cm'
 }
 
-for (const i in Obj) { // forma de printar um Objeto
- // console.log(Obj[i])
-    console.log(`${i} = ${Obj[i]}`) // Desta forma fica mais elegante.
+for (const i in Obj) {
+  // forma de printar um Objeto
+  // console.log(Obj[i])
+  console.log(`${i} = ${Obj[i]}`) // Desta forma fica mais elegante.
 }
