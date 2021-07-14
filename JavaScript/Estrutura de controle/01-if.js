8cbc4ce530be753956eacf8e417b8d2664ebc2ad
Leonardo <leomadeiraa@hotmail.com>
2021-07-13 23:06:52 -0300

    Update 01-if.js

--- JavaScript/Estrutura de controle/01-if.js
+++ JavaScript/Estrutura de controle/01-if.js
@@ -1,25 +1,25 @@
 let x = 10
 
-
 /******************************************************************** */
-                        // COM FUNÇÃO
+// COM FUNÇÃO
 
-function noticiaBoa(nota) { // DUAS FUNÇÕES COM ESTRUTRURA DE CONTROLE
-    if (nota>=7) {
-        console.log('APROVADO!')
-    }
+function noticiaBoa(nota) {
+  // DUAS FUNÇÕES COM ESTRUTRURA DE CONTROLE
+  if (nota >= 7) {
+    console.log('APROVADO!')
+  }
 }
 
-function noticiaRuim(nota){
-    if (nota<=6) {
-        console.log('REPROVADO')
-    }
+function noticiaRuim(nota) {
+  if (nota <= 6) {
+    console.log('REPROVADO')
+  }
 }
 
 noticiaBoa(10)
 
 /******************************************************************** */
 
-if (x===10) {
-    console.log('Hello')
+if (x === 10) {
+  console.log('Hello')
 }
