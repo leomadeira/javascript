c8d1bf5f14e9dc0f3daab264a370c52378f7caab
Leonardo <leomadeiraa@hotmail.com>
2021-07-13 23:06:46 -0300

    Update calculoAgua.js

--- JavaScript/calculoAgua.js
+++ JavaScript/calculoAgua.js
@@ -1,14 +1,14 @@
 function CalcAgua(kg) {
-    let total = 35 * kg
-    total = total / 1000
-    console.log(`Você precisa de ${total.toFixed(3)} L por dia`)
+  let total = 35 * kg
+  total = total / 1000
+  console.log(`Você precisa de ${total.toFixed(3)} L por dia`)
 }
 CalcAgua(200)
 
-function pitagoras(b,c){
-    let a = (b**2)+ (c**2)
-    a = a**0.5 // raizQuadrada
-    console.log(a)
+function pitagoras(b, c) {
+  let a = b ** 2 + c ** 2
+  a = a ** 0.5 // raizQuadrada
+  console.log(a)
 }
 
-pitagoras(3,5)
+pitagoras(3, 5)
