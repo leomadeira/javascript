9eb9d473828993c5c5251996fd7c01f51f7256cc
Leonardo <leomadeiraa@hotmail.com>
2021-07-13 23:07:03 -0300

    Update 05-while.js

--- JavaScript/Estrutura de controle/05-while.js
+++ JavaScript/Estrutura de controle/05-while.js
@@ -1,11 +1,11 @@
-let gerarNumeroAleatorio = (min,max) =>{
-    const valor = Math.random() * (max - min) + min
-    return Math.ceil(valor)
+let gerarNumeroAleatorio = (min, max) => {
+  const valor = Math.random() * (max - min) + min
+  return Math.ceil(valor)
 }
 
 let laco = 0
 
 while (laco != -1) {
-    laco = gerarNumeroAleatorio(-2,10)
-    console.log(laco)
+  laco = gerarNumeroAleatorio(-2, 10)
+  console.log(laco)
 }
