133fa41f496b236fec6147d31fd1e37c002ba754
Leonardo <leomadeiraa@hotmail.com>
2021-07-13 23:06:58 -0300

    Update 03-Else if.js

--- JavaScript/Estrutura de controle/03-Else if.js
+++ JavaScript/Estrutura de controle/03-Else if.js
@@ -1,23 +1,19 @@
-Number.prototype.entre = function (inicio,fim) {
-    return this >= inicio && this <= fim // essa função vai retornar Se (inicio for menor ou igual que this && se this for menor ou igual que fim)
+Number.prototype.entre = function (inicio, fim) {
+  return this >= inicio && this <= fim // essa função vai retornar Se (inicio for menor ou igual que this && se this for menor ou igual que fim)
 }
 
 const imprimirResultado = nota => {
-    if (nota.entre(9,10)) {
-        console.log('ALUNO EXCELENTE, APROVADO!')
-    }
-    else if (nota.entre(7,8.9)) {
-        console.log('APROVADO')
-    }
-    else if (nota.entre(5,6.9)) {
-        console.log('REPROVADO! COM DIREITO A RECUPERAÇÃO')
-    }
-    else if (nota.entre(0,4.9)) {
-        console.log('REPROVADO! SEM DIREITO A RECUPERAÇÃO. ATÉ 2022')
-    }
-    else{
-        console.log('Nota inválida')
-    }
+  if (nota.entre(9, 10)) {
+    console.log('ALUNO EXCELENTE, APROVADO!')
+  } else if (nota.entre(7, 8.9)) {
+    console.log('APROVADO')
+  } else if (nota.entre(5, 6.9)) {
+    console.log('REPROVADO! COM DIREITO A RECUPERAÇÃO')
+  } else if (nota.entre(0, 4.9)) {
+    console.log('REPROVADO! SEM DIREITO A RECUPERAÇÃO. ATÉ 2022')
+  } else {
+    console.log('Nota inválida')
+  }
 }
 
 imprimirResultado(10)
