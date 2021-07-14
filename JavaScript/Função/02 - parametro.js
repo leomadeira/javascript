ddd6cc0f264d4b3036668ae78b82436861d65275
Leonardo <leomadeiraa@hotmail.com>
2021-07-13 23:07:07 -0300

    Update 02 - parametro.js

--- JavaScript/Função/02 - parametro.js
+++ JavaScript/Função/02 - parametro.js
@@ -1,19 +1,18 @@
 // a² = b² + c²
-function calcPitagoras(b,c) { // Parametro é o valor de 'B' e 'C'
-    let a = Math.pow(b,2) + Math.pow(c,2)
-    a = a**(1/2)
-    console.log(a)
+function calcPitagoras(b, c) {
+  // Parametro é o valor de 'B' e 'C'
+  let a = Math.pow(b, 2) + Math.pow(c, 2)
+  a = a ** (1 / 2)
+  console.log(a)
 }
-calcPitagoras(21,28)
+calcPitagoras(21, 28)
 
 // Eu poderia resolver desta outra forma aqui tbm
 
-    const calcPitagoras2 = (b,c,a) => {
-        a = (b**2) + (c**2)
-        a = a**0.5
-        console.log(a)
-    }
+const calcPitagoras2 = (b, c, a) => {
+  a = b ** 2 + c ** 2
+  a = a ** 0.5
+  console.log(a)
+}
 
-calcPitagoras2(3,4)
-
-
+calcPitagoras2(3, 4)
