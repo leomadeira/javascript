1cf96de0d730286b24216a6bf679bf4754bdafb0
Leonardo <leomadeiraa@hotmail.com>
2021-07-18 12:11:17 -0300

    Update boolean.js

--- JavaScript/Fundamentos/boolean.js
+++ JavaScript/Fundamentos/boolean.js
@@ -3,19 +3,19 @@ const y = 15
 const i = 30
 const j = -5
 
-console.log(x>j) // Maior que '>'
-console.log(x<j) // Menor que '<'
-console.log(x>=j) // Maior ou igual '>='
-console.log(x<=j) // Menor ou igual '<='
-console.log(x==x) // Igual '=='
-console.log(x===x) // identico '==='
+console.log(x > j) // Maior que '>'
+console.log(x < j) // Menor que '<'
+console.log(x >= j) // Maior ou igual '>='
+console.log(x <= j) // Menor ou igual '<='
+console.log(x == x) // Igual '=='
+console.log(x === x) // identico '==='
 
 console.log('\n') // \n é um quebra linha
 
-console.log(y>=j && x<=y) // e === &&
-console.log(y>=j || y<=x) // ou === ||
-console.log(!(y>=j)) // ! === NÃO(transforma algo verdadeiro em falso)
-console.log(!(i<=x)) // ! === NÃO(tbm deixa algo falso como verdadeiro)
+console.log(y >= j && x <= y) // e === &&
+console.log(y >= j || y <= x) // ou === ||
+console.log(!(y >= j)) // ! === NÃO(transforma algo verdadeiro em falso)
+console.log(!(i <= x)) // ! === NÃO(tbm deixa algo falso como verdadeiro)
 
 console.log('\n') // \n é um quebra linha
 
