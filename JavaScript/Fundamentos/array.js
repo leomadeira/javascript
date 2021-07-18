1b4746625e727057ac04abb06faec3097cc8d84d
Leonardo <leomadeiraa@hotmail.com>
2021-07-18 12:11:12 -0300

    Update array.js

--- JavaScript/Fundamentos/array.js
+++ JavaScript/Fundamentos/array.js
@@ -1,15 +1,15 @@
-const array = [1,2,3,4] // array sempre fica entre >[]< colchetes
+const array = [1, 2, 3, 4] // array sempre fica entre >[]< colchetes
 
 console.log(array[0]) // printa o primeiro valor da array(que no caso é o numero 1)//lembrando que sempre começa com 0
 
 array[4] = 5 // coloquei mais um numero no indice 4 o valor 5 na array
 console.log(array) // aqui mostra que um novo valor foi colocado
 
-console.log(array.length)//mostra o ultimo valor da array
+console.log(array.length) //mostra o ultimo valor da array
 
 array.push(6) // esta empurrando um novo valor na array. push ele joga no ultimo espaço da array, diferente do primeiro, que pode escolher em que fila o numero 5 pode ficar
 
-console.log(array.length)//mostra o ultimo valor da array
+console.log(array.length) //mostra o ultimo valor da array
 
 console.log(array)
 
