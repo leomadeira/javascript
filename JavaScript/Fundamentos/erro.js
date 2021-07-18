0df835664b5943340ace8f5d9e005cc001812594
Leonardo <leomadeiraa@hotmail.com>
2021-07-18 12:11:23 -0300

    Update erro.js

--- JavaScript/Fundamentos/erro.js
+++ JavaScript/Fundamentos/erro.js
@@ -1,21 +1,24 @@
 function tratarErro(erro) {
-    throw{
-        name: erro.name,
-        msg: erro.message,
-        date: new Date
-    }
+  throw {
+    name: erro.name,
+    msg: erro.message,
+    date: new Date()
+  }
 }
 
 function imprimirNome(obj) {
-    try{ // try: caso o programa nao tenha erro, o programa é executado normalmente
-        console.log(obj.name.toUpperCase() + '!!!')
-    } catch (e){ // catch: caso o programa de erro, a função tratarErro é executada
-        tratarErro(e)
-    } finally{  // finally: mesmo o programa dando erro ou nao o comendo é executado.
-        console.log('Final do Programa!')
-    }
+  try {
+    // try: caso o programa nao tenha erro, o programa é executado normalmente
+    console.log(obj.name.toUpperCase() + '!!!')
+  } catch (e) {
+    // catch: caso o programa de erro, a função tratarErro é executada
+    tratarErro(e)
+  } finally {
+    // finally: mesmo o programa dando erro ou nao o comendo é executado.
+    console.log('Final do Programa!')
+  }
 }
 
-const obj = { n: 'Leonardo'} // Escreva name no lugar de N para o programa executar corretamente.
+const obj = { n: 'Leonardo' } // Escreva name no lugar de N para o programa executar corretamente.
 
-imprimirNome(obj) 
+imprimirNome(obj)
