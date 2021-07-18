8cb8599b167a1d6a89c9f036b992fd143a1a4894
Leonardo <leomadeiraa@hotmail.com>
2021-07-18 12:11:20 -0300

    Update destructuring2.js

--- JavaScript/Fundamentos/destructuring2.js
+++ JavaScript/Fundamentos/destructuring2.js
@@ -3,10 +3,13 @@
 const [a] = [10] // array simples com destructuring
 console.log(a)
 
-const [b, c, d, e] = [4,6,7,2]
+const [b, c, d, e] = [4, 6, 7, 2]
 console.log(b) // B pegou apenas o primeiro elemento
 // observe que desta forma, criamos varios nomes de viriaveis e varios dados. assim não precisamos criar varios consts.
 
-const [[x,y],[j,i]] = [[9,4],[5,3]] // aqui um ex simples de uma matriz
+const [[x, y], [j, i]] = [
+  [9, 4],
+  [5, 3]
+] // aqui um ex simples de uma matriz
 console.log(x)
 console.log(i)
