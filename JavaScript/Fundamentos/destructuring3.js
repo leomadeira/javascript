0425d887d9037dcb3e64434735bc6189a57c621a
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-08-04 23:13:14 -0300

    Update destructuring3.js

--- JavaScript/Fundamentos/destructuring3.js
+++ JavaScript/Fundamentos/destructuring3.js
@@ -1,6 +1,5 @@
-    function roda(min=0,max=100) {
-     let x = Math.random()*(max-min) + min
-      return Math.floor(x)
-    }
-    console.log(roda())
-
+function roda(min = 0, max = 100) {
+  let x = Math.random() * (max - min) + min
+  return Math.floor(x)
+}
+console.log(roda())
