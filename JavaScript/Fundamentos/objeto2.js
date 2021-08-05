41856269bedf62caa5fb313bf3dc4a62b4299527
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-08-04 23:13:21 -0300

    Update objeto2.js

--- JavaScript/Fundamentos/objeto2.js
+++ JavaScript/Fundamentos/objeto2.js
@@ -1,6 +1,6 @@
 /************************* */
 function sobreNome(nome) {
-    console.log(`${nome} Madeira`)
+  console.log(`${nome} Madeira`)
 }
 
 sobreNome('Leonardo')
@@ -9,8 +9,14 @@ console.log(typeof sobreNome)
 
 /************************* */
 
-const cliente = { nome:'José', idade:80, peso:70, altura: 1.55, endereco:{cidade:'Macapá', estado:'Amapá'} }
-console.log(typeof cliente);
+const cliente = {
+  nome: 'José',
+  idade: 80,
+  peso: 70,
+  altura: 1.55,
+  endereco: { cidade: 'Macapá', estado: 'Amapá' }
+}
+console.log(typeof cliente)
 
 /************************* */
 
@@ -22,7 +28,7 @@ console.log(obj1)
 /************************* */
 
 function NewObj(nome) {
-    this.nome = nome
+  this.nome = nome
 }
 newobj1 = new NewObj('Dayane')
 newobj2 = new NewObj('Carlos')
