39390984e0ae1ce3c23c3d422d55e5854ac7b43b
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-08-04 23:13:28 -0300

    Update strings.js

--- JavaScript/Fundamentos/strings.js
+++ JavaScript/Fundamentos/strings.js
@@ -1,12 +1,11 @@
 const nome = 'Leonardo'
 
 console.log(nome.charAt(4)) // vai me da o 4° número da variável nome(letra a) // COMEÇA COM O NUMERO '0'
-console.log(nome.toUpperCase()) // TODAS AS LETRAS EM MAIÚSCULA 
+console.log(nome.toUpperCase()) // TODAS AS LETRAS EM MAIÚSCULA
 console.log(nome.toLowerCase()) // todas as letras em minúscula
 
-
-const nome2 = "Maria" //
-const nome3 = `José`  // Podemos usar tres sinais para declarar uma string. com apenas uma 'x', duas "x" e `x`(crase)
+const nome2 = 'Maria' //
+const nome3 = `José` // Podemos usar tres sinais para declarar uma string. com apenas uma 'x', duas "x" e `x`(crase)
 //crase é o meu preferido para eu poder concatenar nem problemas as variaveis com outros elementos.
 
 console.log(nome3 + ' Monteiro') // Não curto muito dessa forma
@@ -15,8 +14,9 @@ console.log(`${nome} Madeira`) // Dessa forma acho mais profissional e simples. 
 
 const upper = texto => texto.toUpperCase() // criei uma função que deixa qualquer palavra com letra maiuscula
 
-function lower(texto) {  // outra forma de criar função, deixando todas as letras em minusculo
-    return texto.toLowerCase()
+function lower(texto) {
+  // outra forma de criar função, deixando todas as letras em minusculo
+  return texto.toLowerCase()
 }
 
 console.log(upper(nome))
