84d7a537a96f7f7558833ed2471d270ae119964b
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-10 23:57:12 -0300

    Update 06 - doWhile.js

--- JavaScript/Estrutura de controle/06 - doWhile.js
+++ JavaScript/Estrutura de controle/06 - doWhile.js
@@ -1,12 +1,12 @@
-let gerarNumeroAleatorio = (min, max) => {
-  const valor = Math.random() * (max - min) + min
-  return Math.floor(valor)
+let generateRandomNumber = (min, max) => {
+  const value = Math.random() * (max - min) + min
+  return Math.floor(value)
 }
 
-let laco = -1
+let loop = -1
 
 do {
-  // a diferança do while para o doWhale, é o posição da tag, mas o do executa sempre mesmo primeiro mesmo se a condição ja for verdadeira. DoWhile nao é mt utilizado, o que os Devs mais usam é o for que vamos ver agora
-  laco = gerarNumeroAleatorio(-2, 10)
-  console.log(laco)
-} while (laco != -1)
+// the difference from while to doWhale is the tag's position, but it always executes it first if the condition is already true. DoWhile is not used mt, what Devs use the most is what we'll see now
+  loop = generateRandomNumber (-2, 10)
+  console.log(loop)
+} while (loop != -1)
