451a5cb306793ee49377d6fbe152478bc33bff59
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-21 20:42:38 -0300

    Create intro.js

---
+++ JavaScript/AJAX/intro.js
@@ -0,0 +1,12 @@
+//AJAX  API
+//Asynchronous Javascript and XML(Javascript assíncrono e XML)
+//Requisições depois da página ter finalizado seu carregamento
+//Nãoi é necessario atualizar a página
+
+//Como utilizamos?
+//Através de funções de JAVASCRIPT
+    //*fetch(), XMLHttpRequest() e etc...
+//Atualemnte é mais facil de utilizar ajax do que antigamente!
+
+//API FACK so para fazer requisição e ter resposta
+//https://jsonplaceholder.typicode.com/
