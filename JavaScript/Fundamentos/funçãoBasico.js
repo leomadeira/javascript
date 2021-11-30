162ef1f1413fd56000e5ccb4350b5f45cf8d3de7
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-29 22:34:55 -0300

    Update funçãoBasico.js

--- JavaScript/Fundamentos/funçãoBasico.js
+++ JavaScript/Fundamentos/funçãoBasico.js
@@ -9,7 +9,6 @@ function soma2(x, y) {
 soma2(5, 5) // chamando a função
 
 /***************************************************************/
-
 const imprimirSoma = function (x, y) {
   // mais uma forma de criar uma função
   console.log(x + y)
@@ -18,11 +17,9 @@ imprimirSoma(50, 50)
 
 /***************************************************************/
 /***********  Outra forma de criar função(arrow)   *************/
-
 let soma3 = (x, y) => x + y // farma abreviada para criar uma função
 console.log(soma3(4, 4))
 
 let soma4 = (x, y) => console.log(`${x} + ${y} = ${x + y}`) // forma que eu recomendo criar uma função, mais simples e objtivo
 soma4(15, 15)
-
 // esse tipo de função deve ser criado quando a função so deve ter uma linha, e nao precisa colocar um return para retornar o valor. desta forma a função ja retorna automaticamente sem as chaves('{}').
