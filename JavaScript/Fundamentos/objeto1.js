6aad9991d975f9e1e70ea26f94561edf0fa9aa7a
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-03 23:04:39 -0300

    Update objeto1.js

--- JavaScript/Fundamentos/objeto1.js
+++ JavaScript/Fundamentos/objeto1.js
@@ -4,4 +4,5 @@ console.log(pessoa.nome)
 
 const produto = { nome: 'Celular', preco: 1499.99 }
 produto.desconto = produto.preco - (produto.preco * 0.2).toFixed(2) // calculo para da um desconto de 20%
+
 console.log(produto)
