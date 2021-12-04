2c963e092869cd026e81c27c79d96466c4d8119a
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-03 23:04:24 -0300

    Update var-let-const.js

--- JavaScript/Fundamentos/var-let-const.js
+++ JavaScript/Fundamentos/var-let-const.js
@@ -1,9 +1,7 @@
 // Existe três tipos de variáveis(var, let e const)
 
 var num1 = 10 // Var está no escopo global
-
 let num2 = 20 // Let está no escopo local(Regra: sempre use let ao invés de usar 'var')
-
 const num3 = 30.7545456 // Caso não é necessário mudar o valor de uma variável, por boa prática é melhor usar 'const'
 
 /***********************************************************************************/
@@ -13,8 +11,6 @@ num2 = 40 // não pode repetir o let, apenas colocar o num2 e o novo valor da va
 //num3 = 50 // não dá para mudar o valor de uma constante, ela sempre será 30.
 
 console.log(num1, num2)
-
 console.log(typeof num1) //''typeof'' diz qual é o tipo da variavel(se é Number, string, boolean)
-
 console.log(num3.toFixed(2)) // toFixed reduz a quantidade de casas decimais o teu print vai ter. no caso eu coloquei 2 e so vai aparecer 30.75.
 console.log(num3.toString(2)) // toString(2) transforma o valor em binário
