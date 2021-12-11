f43aa520a4153b074c689a8abbe08fba2664f27d
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-10 23:59:07 -0300

    Update class1.js

--- JavaScript/ES6/class/class1.js
+++ JavaScript/ES6/class/class1.js
@@ -1,7 +1,6 @@
 // Uma classe é um tipo de função, mas em vez de usar a palavra function- chave para iniciá-la, usamos a palavra-chave classe as propriedades são atribuídas dentro de um constructor()método.
 
 //Constructor de classe simples
-
 class Car {
     constructor(name){  //constructor(CONSTRUTOR)
         this.brand = name; // brand(MARCA)
@@ -12,11 +11,7 @@ class Car {
 //Observe o caso do nome da classe. Começamos o nome, "Carro", com um caractere maiúsculo. Esta é uma convenção de nomenclatura padrão para classes.
 
 //Crie um objeto chamado "mycar" com base na classe Car:
-
 const mycar = new Car("Lamborghini");
-
-
-
 
 //****************************************************************************************************************************/
 //# Herança de classe #//
