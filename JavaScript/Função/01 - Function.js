49a289c080348e91ffd0c8c2eebaba4437e073a9
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-10 23:58:16 -0300

    Update 01 - Function.js

--- JavaScript/Função/01 - Function.js
+++ JavaScript/Função/01 - Function.js
@@ -4,14 +4,12 @@
     return `Qualquer coisa` // chamando essa função ela retorna essa string
   } // anonimo pq dentro de () não tem nada
 }
-
 // function anonimo literal dentro de uma variavel
 {
   let nomeDaFuncaoAnonimoLiteral = function () {
     /***/
   } // esse é um jeito bacana, nao uso muito
 }
-
 
 // tbm podemos armazenar em um objeto
 const Obj = {}
@@ -22,7 +20,6 @@ Obj.falar = function () {
 console.log(Obj.falar())
 
 /***********************************/
-
 // passar função como parametro
 function primeiraFunc(segundaFunc) {
   segundaFunc()
@@ -35,6 +32,3 @@ primeiraFunc(function () {
 /****************************************************/
 //Arrow function:
 let nomeDaVariavel = (x, y) => x + y // essa é a forma que acho mais interessante para criar uma função. Lembrando que criando desssa forma, não precisamos colocar Return e nem chaves {}
-
-
-
