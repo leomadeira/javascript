e13ff2274b64ac8e3c8c68a3f28086c738ec4e57
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-13 07:02:37 -0300

    Update 06 - doWhile.js

--- JavaScript/Estrutura de controle/06 - doWhile.js
+++ JavaScript/Estrutura de controle/06 - doWhile.js
@@ -4,7 +4,6 @@ let generateRandomNumber = (min, max) => {
 }
 
 let loop = -1
-
 do {
 // the difference from while to doWhale is the tag's position, but it always executes it first if the condition is already true. DoWhile is not used mt, what Devs use the most is what we'll see now
   loop = generateRandomNumber (-2, 10)
