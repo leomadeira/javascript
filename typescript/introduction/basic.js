17064f86c3e380edcda82a198e10feb38b74529f
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-01-15 23:00:22 -0300

    Update basic.js

--- typescript/introduction/basic.js
+++ typescript/introduction/basic.js
@@ -1,2 +1,12 @@
-var a = 'Variavel em TS!';
+"use strict";
+const a = 'Variavel em TS! v2';
 console.log(a);
+//digite no terminal o seguinte: tsc basic.ts
+//com esse comando será criado uma pasta .js para poder compilar o codigo
+//instale ts-node
+//sudo npm i -g ts-node
+//instale a expansão Code Runner
+//CTRL + ALT + N
+//e será compilado o codigo em TS mesmo que não tera problemas, não esqueça de salvar o codigo senão vai
+//comando tsc -w
+//salva o arquivo ts para js automaticamente! extremamente util.
