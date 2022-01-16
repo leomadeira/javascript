5b91b2bdde9c0c375b30f9cabfe25c7af4fadd87
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-01-15 23:00:31 -0300

    Create types.ts

---
+++ typescript/types/types.ts
@@ -0,0 +1,32 @@
+//string
+let nome: string = 'Léo'
+// nome = 25
+console.log(nome)
+
+
+//numbers
+let idade: number = 25
+idade = 24.5
+console.log(idade)
+
+//boolean
+let possuiHobbies: boolean = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
+
+//array
+let hobbies: any[] = ["Jogos","Cozinhar","Correr"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [100, 20, 30]
+console.log(hobbies)
+
+//tuplas
+
+let endereco: [string, number] = ["Av. Lorem Ipsum", 144]
+endereco = ["Rua Lorem Ipsum", 563]
+console.log(endereco)
+
+//enums
+
