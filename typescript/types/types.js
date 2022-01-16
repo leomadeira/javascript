cf76b5a3023aa3434dbfc58666f5dd84a6baffba
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-01-15 23:00:28 -0300

    Create types.js

---
+++ typescript/types/types.js
@@ -0,0 +1,24 @@
+"use strict";
+//string
+let nome = 'Léo';
+// nome = 25
+console.log(nome);
+//numbers
+let idade = 25;
+idade = 24.5;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+// possuiHobbies = 1
+console.log(possuiHobbies);
+//array
+let hobbies = ["Jogos", "Cozinhar", "Correr"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 20, 30];
+console.log(hobbies);
+//tuplas
+let endereco = ["Av. Lorem Ipsum", 144];
+endereco = ["Rua Lorem Ipsum", 563];
+console.log(endereco);
+//enums
