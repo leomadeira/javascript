be50fa52badf404cdefbab23a655c56ba585f274
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-03-20 23:30:27 -0300

    Update 01 - Function.js

--- javascript/Função/01 - Function.js
+++ javascript/Função/01 - Function.js
@@ -11,13 +11,14 @@
   } // esse é um jeito bacana, nao uso muito
 }
 // tbm podemos armazenar em um objeto
+
 const Obj = {}
 Obj.falar = function () {
   return 'Olá, mundo!'
 }
 console.log(Obj.falar())
-/***********************************/
 
+/***********************************/
 // passar função como parametro
 
 function primeiraFunc(segundaFunc) {
@@ -27,7 +28,7 @@ function primeiraFunc(segundaFunc) {
 primeiraFunc(function () {
   console.log('Executando...')
 })
-/***************************************************/
 
+/***************************************************/
 //Arrow function:
 let nomeDaVariavel = (x, y) => x + y // essa é a forma que acho mais interessante para criar uma função. Lembrando que criando desssa forma, não precisamos colocar Return e nem chaves {}
